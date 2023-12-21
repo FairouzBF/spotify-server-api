cc68eba99d20f88a7cf0eb78728a9a828b4ac3f3
Saelyn <ben.fraj.fairouz@gmail.com>
2023-12-21 21:10:14 +0100

    add mongo connexion

--- src/app.js
+++ src/app.js
@@ -1,7 +1,30 @@
 const express = require('express')
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express()
 const PORT = 4000
+
+// Log environment variables
+console.log('MONGODB_USER:', process.env.MONGODB_USER);
+console.log('MONGODB_PASSWORD:', process.env.MONGODB_PASSWORD);
+console.log('MONGODB_CLUSTER:', process.env.MONGODB_CLUSTER);
+console.log('PORT:', process.env.PORT);
+
+mongoose.set('strictQuery', false);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}${process.env.MONGODB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority`,
+    {
+      serverSelectionTimeoutMS: 50000,
+      connectTimeoutMS: 30000, // 30 seconds
+      socketTimeoutMS: 45000,  // 45 seconds
+    }
+  )
+  .then(() => {
+    console.log(`Successfully connect to database`);
+  })
+  .catch(err => console.log(err));
 
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `)
